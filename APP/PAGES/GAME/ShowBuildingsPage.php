1e0ae22dbfe134feda43e9918d00d991b581dbd1
xnovaptteam <xnovaptteam@0f9018d3-30b7-49af-881b-885797e25b4b>
2013-10-09 20:40:57 +0000

    - Correção das licensas nos ficheiros adicionados. nos comitts anteriores.

--- APP/PAGES/GAME/ShowBuildingsPage.php
+++ APP/PAGES/GAME/ShowBuildingsPage.php
@@ -1,8 +1,26 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowBuildingsPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  05/Out/2013 14:24:19
  */
 
 /**
