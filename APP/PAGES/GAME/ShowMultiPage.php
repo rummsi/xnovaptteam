9196e4a846cc82b94f3ef2064a313f98acdfc302
xnovaptteam <xnovaptteam@0f9018d3-30b7-49af-881b-885797e25b4b>
2014-03-22 22:59:10 +0000

    - Conversão do add_declare para smarty template engine.

---
+++ APP/PAGES/GAME/ShowMultiPage.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * Expression package is undefined on line 4, column 19 in file:///C:/UniServerZ/www/xnovaptteam/nbproject/licenseheader.txt.
+ * Copyright (C) 2012
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * Expression package is undefined on line 19, column 19 in file:///C:/UniServerZ/www/xnovaptteam/nbproject/licenseheader.txt.
+ * @Rui Silva
+ * @ShowMultiPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @Expression version is undefined on line 23, column 20 in file:///C:/UniServerZ/www/xnovaptteam/nbproject/licenseheader.txt.  22/Mar/2014 21:58:58
+ */
+
+/**
+ * Description of ShowMultiPage
+ *
+ * @author Rui Silva
+ */
+class ShowMultiPage extends AbstractGamePage {
+
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'multi';
+    }
+
+    function show() {
+        global $lang, $title;
+
+        includeLang('multi');
+
+        $Mode = HTTP::_GP('mode', '');
+
+        $this->tplObj->assign(array(
+            'title' => $lang['multi_declare'],
+        ));
+
+        $this->render('multi.default.tpl');
+    }
+
+    function addit() {
+        global $lang, $title, $user;
+
+        includeLang('multi');
+        $Mode = HTTP::_GP('mode', '');
+        
+        $declarator = $user['id'];
+        $declarator_name = addslashes(htmlspecialchars($user['username']));
+        $decl1 = addslashes(htmlspecialchars($_POST['dec1']));
+        $decl2 = addslashes(htmlspecialchars($_POST['dec2']));
+        $decl3 = addslashes(htmlspecialchars($_POST['dec3']));
+        $reason1 = addslashes(htmlspecialchars($_POST['reason']));
+
+        $QryDeclare = "INSERT INTO {{table}} SET ";
+        $QryDeclare .= "`declarator` = '" . $declarator . "', ";
+        $QryDeclare .= "`declarator_name` = '" . $declarator_name . "', ";
+        $QryDeclare .= "`declared_1` = '" . $decl1 . "', ";
+        $QryDeclare .= "`declared_2` = '" . $decl2 . "', ";
+        $QryDeclare .= "`declared_3` = '" . $decl3 . "', ";
+        $QryDeclare .= "`reason`     = '" . $reason1 . "' ";
+
+        doquery($QryDeclare, "declared");
+        doquery("UPDATE {{table}} SET multi_validated ='1' WHERE username='{$user['username']}'", "users");
+
+        AdminMessage($lang['multi_message_content'], $lang['multi_message_add']);
+    }
+
+}
---
+++ language/fr/multi.mo
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of XNova:Legacies
+ *
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @see http://www.xnova-ng.org/
+ *
+ * Copyright (c) 2009-Present, XNova Support Team <http://www.xnova-ng.org>
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *                                --> NOTICE <--
+ *  This file is part of the core development branch, changing its contents will
+ * make you unable to use the automatic updates manager. Please refer to the
+ * documentation for further information about customizing XNova.
+ *
+ */
+
+
+$lang['multi_declare'] = 'DECLARATION DE MULTICOMPTE';
+$lang['fst_player_imp'] = "Premier joueur implique";
+$lang['scd_player_imp'] = "Second joueur implique";
+$lang['trd_player_imp'] = "Troisieme joueur implique";
+$lang['multi_motive'] = "Raison du multicompte";
+
+$lang['multi_message_content'] = "Merci, votre demande a ete prise en compte. Les autres joueurs que vous avez implique doivent egalement et imperativement suivre cette procedure aussi.<br><a href='game.php?page=multi'>Retour</a>";
+$lang['multi_message_add'] = "Ajout";
+?>
