f5e6e9f88b97d8af1760372c88561b2be6ec2221
xnovaptteam <xnovaptteam@0f9018d3-30b7-49af-881b-885797e25b4b>
2014-06-06 20:02:27 +0000

    - Ficheiros referentes á aplicação agora geridos pelo configs/app_files.php.

---
+++ trunk/APP/configs/app_files.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @app_files.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  6/Jun/2014 20:56:20
+ */
+
+include(ROOT_PATH . 'APP/CLASSES/Galaxy.php');
+include(ROOT_PATH . 'APP/CLASSES/Display.php');
+include(ROOT_PATH . 'APP/CLASSES/Game.php');
+include(ROOT_PATH . 'APP/CLASSES/Templates.php');
+include(ROOT_PATH . 'APP/CLASSES/HTTP.php');
