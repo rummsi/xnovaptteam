8d963cc7f975f165861f8b95e517fa9aec338dd7
rummsi <rummsi@gmail.com>
2024-05-30 12:26:52 +0100

    - Actualização do smarty.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'vendor/ptgamespt2022',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '95a203662f880370491b48554968d880e51f1ab5',
+        'reference' => '9b99913888f750891ad7654e860ea4ac7be42e52',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'smarty/smarty' => array(
-            'pretty_version' => 'v4.3.0',
-            'version' => '4.3.0.0',
-            'reference' => 'c02e9e135ea719b91f457a0072748ded0e852e7d',
+            'pretty_version' => 'v4.5.3',
+            'version' => '4.5.3.0',
+            'reference' => '9fc96a13dbaf546c3d7bcf95466726578cd4e0fa',
             'type' => 'library',
             'install_path' => __DIR__ . '/../smarty/smarty',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'vendor/ptgamespt2022' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '95a203662f880370491b48554968d880e51f1ab5',
+            'reference' => '9b99913888f750891ad7654e860ea4ac7be42e52',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
